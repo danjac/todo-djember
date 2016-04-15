f833d5e11f57a8ae1b1ed3540d8498cd6f931f98
Dan Jacob <danjac354@gmail.com>
2016-04-15 20:36:48 +0300

    Some setting fixes

--- todo_django/todo_django/settings.py
+++ todo_django/todo_django/settings.py
@@ -155,7 +155,18 @@ REST_FRAMEWORK = {
 
 SOCIAL_AUTH_LINKEDIN_OAUTH2_KEY = '77wfj2kjly3f1d'
 SOCIAL_AUTH_LINKEDIN_OAUTH2_SECRET = 'Ylz4l23qTf5i8zsU'
-SOCIAL_AUTH_LINKEDIN_OAUTH2_SCOPE = ['r_emailaddress']
+SOCIAL_AUTH_LINKEDIN_OAUTH2_SCOPE = ['r_basicprofile', 'r_emailaddress']
+
+
+# Add the fields so they will be requested from linkedin.
+SOCIAL_AUTH_LINKEDIN_OAUTH2_FIELD_SELECTORS = ['email-address', 'headline', 'industry']
+# Arrange to add the fields to UserSocialAuth.extra_data
+SOCIAL_AUTH_LINKEDIN_OAUTH2_EXTRA_DATA = [('id', 'id'),
+                                   ('firstName', 'first_name'),
+                                   ('lastName', 'last_name'),
+                                   ('emailAddress', 'email_address'),
+                                   ('headline', 'headline'),
+                                   ('industry', 'industry')]
 
 """
 SOCIAL_AUTH_PIPELINE = (
